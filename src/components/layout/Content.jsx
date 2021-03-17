db1730b6ed77f011062ca195ba486d6242f2e128
Luicil Fernandes <luicilfernandes@hotmail.com>
2021-03-17 11:50:58 -0300

    Fim

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import './Content.css'
-import{Switch, Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import About from '../../views/samples/About'
 import Home from '../../views/samples/Home'
 import Param from '../../views/samples/Param'
+import NotFound from '../../views/samples/NotFound'
 
 const Content = props => {
     return (
         <main className="Content">
-            <Switch>
-                <Route exact path="/">
-                    <Home></Home>
-                </Route>
-            </Switch>            
             <Switch>
                 <Route exact path="/about">
                     <About></About>
@@ -26,6 +22,16 @@ const Content = props => {
             <Switch>
                 <Route exact path="/param">
                     <Param></Param>
+                </Route>
+            </Switch>
+            <Switch>
+                <Route path="/">
+                    <Home></Home>
+                </Route>
+            </Switch>
+            <Switch>
+                <Route path="*">
+                    <NotFound />
                 </Route>
             </Switch>
         </main>
